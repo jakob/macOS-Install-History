017e066fe2097cffb4e7cf73f07e9b0c9c9e1d11
Jakob Egger <jakob@eggerapps.at>
2022-05-06 14:25:30 +0200

    Implement basic features

--- macOS Install History/AppDelegate.swift
+++ macOS Install History/AppDelegate.swift
@@ -8,13 +8,81 @@
 import Cocoa
 
 @main
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTableViewDelegate {
 
 	@IBOutlet var window: NSWindow!
 
+	@IBOutlet weak var pathField: NSTextField!
+	
+	@IBAction func pathDidChange(_ sender: Any) {
+		do {
+			let url = URL(fileURLWithPath: pathField.stringValue)
+			let newHistory = try InstallHistory(url: url)
+			history = newHistory
+		} catch let error {
+			history = nil
+			NSApp.presentError(error, modalFor: window, delegate: nil, didPresent: nil, contextInfo: nil)
+		}
+		historyTableView.reloadData()
+	}
+	
+	@IBAction func choosePath(_ sender: Any) {
+		let openDialog = NSOpenPanel()
+		openDialog.beginSheetModal(for: window) { response in
+			if response == .OK {
+				self.pathField.stringValue = openDialog.url!.path
+				do {
+					let newHistory = try InstallHistory(url: openDialog.url!)
+					self.history = newHistory
+				} catch let error {
+					self.history = nil
+					NSApp.presentError(error, modalFor: self.window, delegate: nil, didPresent: nil, contextInfo: nil)
+				}
+				self.historyTableView.reloadData()
+			}
+		}
 
+	}
+	@IBOutlet weak var historyTableView: NSTableView!
+	
+	var history: InstallHistory?
+	
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
+		do {
+			let newHistory = try InstallHistory.forCurrentMachine()
+			history = newHistory
+			pathField.stringValue = newHistory.url.path
+			historyTableView.reloadData()
+		} catch let error {
+			NSApp.presentError(error, modalFor: window, delegate: nil, didPresent: nil, contextInfo: nil)
+		}
+	}
+	
+	func numberOfRows(in tableView: NSTableView) -> Int {
+		return history?.records.count ?? 0
+	}
+	
+	func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+		guard let tableColumn = tableColumn else {
+			return nil
+		}
+
+		let formatter = DateFormatter()
+		formatter.dateStyle = .medium
+		formatter.timeStyle = .none
+		
+		let view = tableView.makeView(withIdentifier: .init(rawValue: (tableColumn.identifier.rawValue + "Cell")), owner: self) as! NSTableCellView
+		switch tableColumn.identifier {
+		case .init(rawValue: "date"):
+			view.textField?.stringValue = formatter.string(from: history!.records[row].installDate)
+		case .init(rawValue: "displayVersion"):
+			view.textField?.stringValue = history!.records[row].displayVersion
+		case .init(rawValue: "displayName"):
+			view.textField?.stringValue = history!.records[row].displayName
+		default:
+			print("Unknoen table column: \(tableColumn)")
+		}
+		return view
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
